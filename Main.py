5c98a758b0cad0e58ac292b93e4db70fc3c2ffa1
Muhammad Farhan Tanvir <39805495+muhammadfarhantanvir@users.noreply.github.com>
2024-04-15 17:59:25 +0200

    Main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import *
+from sklearn.linear_model import *
+from sklearn.metrics import *
 from sklearn.preprocessing import StandardScaler
 import joblib  # Import joblib for model saving/loading
 import matplotlib.pyplot as plt
